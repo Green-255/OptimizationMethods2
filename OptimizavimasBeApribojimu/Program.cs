786f5ac5652f6ce7923e17bfb9a91d599f678bec
nei nei <naglis.kontautas04@gmail.com>
2024-11-24 14:56:11 +0200

    Add project files.

---
+++ OptimizavimasBeApribojimu/Program.cs
@@ -0,0 +1,272 @@
+﻿/*
+This is a program that excecutes three types of optimization algorithms
+    tracks the number of cycles and goal (objective) function calls so that
+    the algorithms' performance could be compared with eachother.
+The algorithms are:
+    - Gradient Descent
+    - Fastest Descent, that uses golden section search to find the optimal step (lambda) size
+    - Deformed Simplex - Nelder-Mead algorithm
+
+Notes:
+    - this is a study laboratory work, so the user interface is minimal.
+    - this the results are printed to the console.
+    - the 3D and simplex plots are displayed in a separate window.
+    - to run the algorithms, the appropraite lines in main must be uncommented.
+    - the objective function is defined due to the specific task. IT CAN BE CHANGED
+        TO ANY OTHER FUNCTION, however, the partial gradients must be adjusted accordingly.
+    - the gradient descent and fastest descent algorithms are implemented in
+        the Program class, while the Simplex is defined in the sepereate Simplex class.
+ */
+
+using Plotly.NET;
+
+namespace OptimizavimasBeApribojimu
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // Define the coordinates of a starting point.
+            double x = 0;
+            double y = 0;
+            x = 1;
+            y = 1;
+
+            double epsilon = 1e-5; // variable that defines end of algorithms.
+            Program program = new Program();
+
+
+            //(List<(double, double, double)> pointTimeLine, int functionCallsCount, int cycles) = program.gradientDescent(x, y, epsilon);
+            (List<(double, double, double)> pointTimeLine, int functionCallsCount, int cycles) = program.FastestDescent(x, y, epsilon);
+
+            foreach (var point in pointTimeLine)
+            {
+                Console.WriteLine($"X: {point.Item1}, Y: {point.Item2}, Žingsnio ilgis: {point.Item3}");
+            }
+            program.Display3D(pointTimeLine);
+
+            //Simplex simplex_ = new Simplex();
+            //(List<double[][]> simplexTimeLine, int functionCallsCount, int cycles) = simplex_.Run(x, y, epsilon);
+            //program.DisplaySimplexTimeline(simplexTimeLine);
+
+            Console.WriteLine($"\nTikslo funkcija kviesta {functionCallsCount} kartų, per {cycles} ciklus.");
+        }
+
+        private (List<(double, double, double)>, int, int) FastestDescent(double x, double y, double epsilon)
+        {
+            List<(double, double, double)> pointTimeLine = new();
+            pointTimeLine.Add((x, y, 1));
+            int cycles = 0;
+            int targetFunctionCalled = 0;
+            double t = (-1 + Math.Sqrt(5)) / 2;
+            double leftBorder = 0;
+            double rightBorder = 5;
+
+            while (cycles < 5000)
+            {
+                (double gradX, double gradY) = PartialGradients(x, y);
+                targetFunctionCalled += 2;
+                if (Math.Abs(gradX) < epsilon && Math.Abs(gradY) < epsilon)
+                {
+                    Console.WriteLine($"Metodas pasibaigė, nes gradientų reikšmės mažesnės" +
+                        $" už epsilon {epsilon}: gradX: {gradX}, gradY: {gradY}\n");
+                    break;
+                }
+
+                double interval = rightBorder - leftBorder;
+                double lambda1 = rightBorder - t * interval; 
+                double lambda2 = leftBorder + t * interval;
+
+                double f1 = ObjectiveFunction(x - lambda1 * gradX, y - lambda1 * gradY);
+                double f2 = ObjectiveFunction(x - lambda2 * gradX, y - lambda2 * gradY);
+                targetFunctionCalled += 2;
+
+                while (interval > epsilon)
+                {
+                    if (f2 < f1)
+                    {
+                        leftBorder = lambda1;
+                        lambda1 = lambda2;
+                        f1 = f2;
+                        interval = rightBorder - leftBorder;
+                        lambda2 = leftBorder + t * interval;
+                        f2 = ObjectiveFunction(x - lambda2 * gradX, y - lambda2 * gradY);
+                        targetFunctionCalled++;
+                    }
+                    else if (f1 < f2)
+                    {
+                        rightBorder = lambda2;
+                        lambda2 = lambda1;
+                        f2 = f1;
+                        interval = rightBorder - leftBorder;
+                        lambda1 = rightBorder - t * interval;
+                        f1 = ObjectiveFunction(x - lambda1 * gradX, y - lambda1 * gradY);
+                        targetFunctionCalled++;
+                    }
+                    else
+                    {
+                        leftBorder = lambda1;
+                        rightBorder = lambda2;
+                        interval = rightBorder - leftBorder;
+                        lambda1 = rightBorder - t * interval;
+                        lambda2 = leftBorder + t * interval;
+                        f1 = ObjectiveFunction(x - lambda1 * gradX, y - lambda1 * gradY);
+                        f2 = ObjectiveFunction(x - lambda2 * gradX, y - lambda2 * gradY);
+                        targetFunctionCalled += 2;
+                    }
+                    cycles++;
+                }
+                double lambda = (f1 < f2) ? lambda1 : lambda2;
+                x -= lambda * gradX;
+                y -= lambda * gradY;
+                pointTimeLine.Add((x, y, lambda));
+                cycles++;
+            }
+
+            return (pointTimeLine, targetFunctionCalled, cycles);
+        }
+
+        private (List<(double, double, double)>, int, int) gradientDescent(double x, double y, double epsilon)
+        {
+            List<(double, double, double)> pointTimeLine = new List<(double, double, double)>();
+            int i = 0; pointTimeLine.Add((x, y, 1));
+            int targetFunctionCalled = 0;
+            while (i < 10000)
+            {
+                (double gradX, double gradY) = PartialGradients(x, y);
+                targetFunctionCalled += 2;
+                if (Math.Abs(gradX) < epsilon && Math.Abs(gradY) < epsilon)
+                {
+                    Console.WriteLine($"Metodas pasibaigė, nes gradientų reikšmės mažesnės" +
+                        $"už epsilon {epsilon}: gradX: {gradX}, gradY: {gradY}\n");
+                    break;
+                }
+                x -= gradX;
+                y -= gradY;
+                pointTimeLine.Add((x, y, 1));
+                i++;
+            }
+            return (pointTimeLine, targetFunctionCalled, i);
+        }
+
+        private double ObjectiveFunction(double x, double y)
+        {
+            return -x * y * (1 - x - y) / 8;
+        }
+        private (double, double) PartialGradients(double x, double y)
+        {
+            double dx = y * (2 * x + y - 1) / 8;
+            double dy = x * (2 * y + x - 1) / 8;
+            return (dx, dy);
+        }
+
+
+        // Displaying the visualization of the optimization path for Gradient descent and Fastest Descent
+        // the Simplex visualization will not work here.
+        private void Display3D(List<(double x, double y, double lambda)> pointTimeLine)
+        {
+            var trace = Chart3D.Chart.Scatter3D<double, double, double, string>(
+                pointTimeLine.Select(p => p.x).ToArray(),
+                pointTimeLine.Select(p => p.y).ToArray(),
+                pointTimeLine.Select(p => ObjectiveFunction(p.x, p.y)).ToArray(),
+                mode: StyleParam.Mode.Lines_Markers,
+                Name: "Optimization Descent Path"
+            )
+            .WithMarkerStyle(
+                Size: 6,
+                Color: Color.fromString("red"),
+                Symbol: StyleParam.MarkerSymbol.Star
+            )
+            .WithLineStyle(
+                Color: Color.fromString("blue"),
+                Dash: StyleParam.DrawingStyle.Solid
+            );
+
+
+            int gridSize = 80;
+            var xRange = Enumerable.Range(0, gridSize).Select(i => i * 1.0 / gridSize).ToArray();
+            var yRange = Enumerable.Range(0, gridSize).Select(i => i * 1.0 / gridSize).ToArray();
+
+            //var zSurface = new double[gridSize][];
+            //for (int i = 0; i < gridSize; i++)
+            //{
+            //    zSurface[i] = new double[gridSize];
+            //    for (int j = 0; j < gridSize; j++)
+            //    {
+            //        zSurface[i][j] = ObjectiveFunction(xRange[i], yRange[j]);
+            //    }
+            //}
+
+            //var zSurface = new List<double>();
+            //foreach (var x in xRange)
+            //{
+            //    foreach (var y in yRange)
+            //    {
+            //        zSurface.Add(ObjectiveFunction(x, y));
+            //    }
+            //}
+
+            //var surfaceTrace = Chart3D.Chart.Surface<double, double, double, string, string>(
+            //    xRange,
+            //    yRange,
+            //    zSurface
+            //)
+            //.WithSurfaceStyle(
+            //    ColorScale: StyleParam.Colorscale.Viridis
+            //);
+
+            //var chart = Chart.Combine(new[] { surfaceTrace, trace })
+            var chart = trace
+                .WithXAxisStyle(Title.init("x"))
+                .WithYAxisStyle(Title.init("y"))
+                .WithZAxisStyle(Title.init("z"))
+                .WithSize(1000, 1000)
+                .WithGeoStyle();
+            chart.Show();
+        }
+
+
+        // Displaying the visualization of the optimization path specifically for Simplex
+        private void DisplaySimplexTimeline(List<double[][]> simplexTimeLine)
+        {
+            var traces = new List<GenericChart>();
+            int i = 0;
+            foreach (var simplex in simplexTimeLine)
+            {
+                i++;
+                List<double> xValues = simplex.Select(p => p[0]).ToList();
+                List<double> yValues = simplex.Select(p => p[1]).ToList();
+                xValues.Add(xValues[0]);
+                yValues.Add(yValues[0]);
+
+
+                Console.WriteLine($"{xValues[0]} & {yValues[0]} & {xValues[1]} & {yValues[1]} & {xValues[2]} & {yValues[2]} \\\\");
+
+                var trace = Chart2D.Chart.Scatter<double, double, string>(
+                    xValues,
+                    yValues,
+                    mode: StyleParam.Mode.Lines_Markers
+                )
+                .WithLineStyle(
+                    Color: Color.fromString("blue"),
+                    Dash: StyleParam.DrawingStyle.Solid,
+                    Width: 1
+                )
+                .WithMarkerStyle(
+                    Size: 5,
+                    Color: Color.fromString("red"),
+                    Symbol: StyleParam.MarkerSymbol.Circle
+                );
+
+                traces.Add(trace);
+            }
+
+            var chart = Chart.Combine(traces)
+                .WithXAxisStyle(Title.init("x"))
+                .WithYAxisStyle(Title.init("y"))
+                .WithSize(1000, 1000);
+
+            chart.Show();
+        }
+    }
+}
---
+++ OptimizavimasBeApribojimu/Simplex.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace OptimizavimasBeApribojimu
+{
+    public class Simplex
+    {
+        public Simplex() { }
+
+        public (List<double[][]>, int, int) Run(double x, double y, double epsilon)
+        {
+            List<double[][]> simplexTimeLine = new();
+            double atspindys = 1;
+            double sumazintasAtspindys = 0.5;
+            double istemptasAtspindys = 2;
+            double edgeSize = 1;
+
+            double[][] simplex = GenerateSimplex(x, y, edgeSize);
+            int functionCallsCount = 3;
+            int cycles = 0;
+
+            while (getLongestEdgeLength(simplex) > epsilon)
+            {
+                Array.Sort(simplex, (a, b) => b[2].CompareTo(a[2]));
+                simplexTimeLine.Add((double[][])simplex.Clone());
+                Console.WriteLine($"F-jos reikšmės: {simplex[0][2]}, {simplex[1][2]}, {simplex[2][2]}");
+                Console.WriteLine($"Simplekso taškai: ({simplex[0][0]}; {simplex[0][1]}) | ({simplex[1][0]}; {simplex[1][1]}) | ({simplex[2][0]}; {simplex[2][1]}) |");
+                cycles++;
+
+                double[] best = simplex[2];
+                double[] worst = simplex[0];
+                double[] secondWorst = simplex[1];
+
+                double[] centroid = CalculateCentroid(simplex, worst);
+                double[] newPoint = Reflect(centroid, worst, atspindys);
+
+                (simplex, int count, bool isNegative) = FixNegative(simplex, newPoint, centroid, -sumazintasAtspindys);
+                functionCallsCount += count;
+                if (isNegative)
+                    continue;
+                newPoint[2] = ObjectiveFunction(newPoint[0], newPoint[1]);
+                functionCallsCount++;
+
+                Console.WriteLine($"Atsipindys: {newPoint[2]}");
+
+                if (newPoint[2] > best[2] && newPoint[2] < secondWorst[2])
+                {
+                    simplex[0] = newPoint;
+                    Console.WriteLine($"liko toks pats: {newPoint[2]}\n");
+                    continue;
+                }
+                else if (newPoint[2] < best[2])
+                {
+                    double[] extendedPoint = Reflect(centroid, worst, istemptasAtspindys);
+                    extendedPoint[2] = ObjectiveFunction(extendedPoint[0], extendedPoint[1]);
+                    functionCallsCount++;
+                    (simplex, count, isNegative) = FixNegative(simplex, newPoint, centroid, -sumazintasAtspindys);
+                    functionCallsCount += count;
+                    if (isNegative)
+                        continue;
+                    simplex[0] = extendedPoint[2] < newPoint[2] ? extendedPoint : newPoint;
+                    Console.WriteLine($"Istemptas: {simplex[0][2]}\n");
+                }
+                else
+                {
+                    double[] compresedPoint = Reflect(centroid, worst, sumazintasAtspindys);
+                    compresedPoint[2] = ObjectiveFunction(compresedPoint[0], compresedPoint[1]);
+                    functionCallsCount++;
+                    if (compresedPoint[2] < secondWorst[2])
+                    {
+                        simplex[0] = compresedPoint;
+                        Console.WriteLine($"Suspaustas: {simplex[0][2]}\n");
+                    }
+                    else
+                    {
+                        (simplex, int calls) = ContractSimplex(simplex, centroid, sumazintasAtspindys);
+                        functionCallsCount += calls;
+                    }
+                }
+            }
+            return (simplexTimeLine, functionCallsCount, cycles);
+        }
+
+        double ObjectiveFunction(double x, double y)
+        {
+            return -x * y * (1 - x - y) / 8;
+        }
+
+        private (double[][], int) ContractSimplex(double[][] simplex, double[] centroid, double phi)
+        {
+            double[] contractedPoint = Reflect(centroid, simplex[0], -phi);
+            contractedPoint[2] = ObjectiveFunction(contractedPoint[0], contractedPoint[1]);
+            int functionCallsCount = 1;
+            if (contractedPoint[2] < simplex[1][2])
+            {
+                simplex[0] = contractedPoint;
+                Console.WriteLine($"Suspaustas: {simplex[0][2]}\n");
+            }
+            else
+            {
+                Console.WriteLine("Simpleksas sumazejo\n");
+                simplex = ShrinkSimplex(simplex);
+                functionCallsCount += 2;
+            }
+            return (simplex, functionCallsCount);
+        }
+
+        private (double[][], int, bool) FixNegative(double[][] simplex, double[] point, double[] centroid, double phi)
+        {
+            int functionCallsCount = 0;
+            bool isNegative = false;
+            if (point[0] < 0 || point[1] < 0)
+            {
+                (simplex, int calls) = ContractSimplex(simplex, centroid, phi);
+                functionCallsCount += calls;
+                isNegative = true;
+            }
+            return (simplex, functionCallsCount, isNegative);
+        }
+
+        private double[][] ShrinkSimplex(double[][] simplex)
+        {
+            double x = simplex[0][0];
+            double y = simplex[0][1];
+            double x2 = (x + simplex[1][0]) / 2;
+            double y2 = (y + simplex[1][1]) / 2;
+            double x3 = (x + simplex[2][0]) / 2;
+            double y3 = (y + simplex[2][1]) / 2;
+            double[][] newSimplex =
+                [
+                    new double[] { x, y, simplex[0][2] },
+                    new double[] { x2, y2, ObjectiveFunction(x2, y2) },
+                    new double[] { x3, y3, ObjectiveFunction(x3, y3) }
+                ];
+            return newSimplex;
+        }
+
+        private double[] CalculateCentroid(double[][] simplex, double[] exclude)
+        {
+            int length = 2; // simplex[0].Length;
+            double[] centroid = new double[length + 1];
+            foreach (var point in simplex)
+            {
+                if (point != exclude)
+                {
+                    for (int i = 0; i < length; i++)
+                        centroid[i] += point[i];
+                }
+            }
+
+            length = 2; //simplex.Length -1;
+            for (int i = 0; i < length; i++)
+                centroid[i] /= length;
+            return centroid;
+        }
+
+        private double[] Reflect(double[] centroid, double[] worst, double phi)
+        {
+            int length = worst.Length;
+            double[] reflected = new double[length];
+            for (int i = 0; i < length - 1; i++)
+                reflected[i] = worst[i] + (1 + phi) * (centroid[i] - worst[i]);
+            return reflected;
+        }
+
+        private double[][] GenerateSimplex(double x, double y, double edgeSize)
+        {
+            double x2 = x + edgeSize;
+            double y2 = y;
+            double x3 = x + edgeSize / 2;
+            double y3 = y + Math.Sqrt(Math.Pow(edgeSize, 2) - Math.Pow(edgeSize / 2, 2));
+
+            double[][] simplex =
+            [
+                new double[] { x, y, ObjectiveFunction(x, y) },
+                new double[] { x2, y2, ObjectiveFunction(x2, y2) },
+                new double[] { x3, y3, ObjectiveFunction(x3, y3) },
+            ];
+            return simplex;
+        }
+
+        private double[][] GenerateSimplex2(double x, double y, double edgeSize)
+        {
+            double beta1 = (Math.Sqrt(3) + 1) / (2 * Math.Sqrt(2)) * edgeSize;
+            double beta2 = (Math.Sqrt(3) - 1) / (2 * Math.Sqrt(2)) * edgeSize;
+            // (√3 + 1) / 2√2 ir (√3 - 1) / 2√2
+
+            double x2 = x + beta1;
+            double y2 = y + beta2;
+            double x3 = x + beta2;
+            double y3 = y + beta1;
+
+            double[][] simplex =
+            {
+                    new double[] { x, y, ObjectiveFunction(x, y) },
+                    new double[] { x2, y2, ObjectiveFunction(x2, y2) },
+                    new double[] { x3, y3, ObjectiveFunction(x3, y3) },
+                };
+            return simplex;
+        }
+
+        private double getLongestEdgeLength(double[][] simplex)
+        {
+            double x = Math.Sqrt(Math.Pow(simplex[0][0] - simplex[1][0], 2) + Math.Pow(simplex[0][1] - simplex[1][1], 2));
+            double y = Math.Sqrt(Math.Pow(simplex[0][0] - simplex[2][0], 2) + Math.Pow(simplex[0][1] - simplex[2][1], 2));
+            double z = Math.Sqrt(Math.Pow(simplex[1][0] - simplex[2][0], 2) + Math.Pow(simplex[1][1] - simplex[2][1], 2));
+
+            return Math.Max(x, Math.Max(y, z));
+        }
+    }
+}
